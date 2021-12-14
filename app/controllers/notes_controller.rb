75d0a8ea40aec3fb685ba918e3d1f94e15851f62
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-14 19:56:14 +0530

    Add functionality for creating new notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,16 @@
 class NotesController < ApplicationController
   def index
-  end  
+    @notes = Note.all
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(notes_params)
+    @note.save
+  end
+
+  private
+  def notes_params
+    params.require(:note).permit(:title, :description)
+  end
 end
