e335df77eb229136e1b74cd645cfa4337a34cff7
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-14 22:13:29 +0530

    Improve layout

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,30 +1,36 @@
 class NotesController < ApplicationController
   def index
     @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
     @note = Note.new
   end
 
   def create
     @note = Note.new(notes_params)
     @note.save
-    redirect_back(fallback_location: root_path)
+    redirect_to @note
   end
 
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 
   def edit
-    render :edit
     @note = Note.find(params[:id])
   end
 
   def update
     @note = Note.find(params[:id])
     @note.update(notes_params)
-    redirect_to root_path
+    redirect_to @note
   end
 
   private
