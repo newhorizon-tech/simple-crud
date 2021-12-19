0560b1df7a6304729272d2b29116a3ebd945a5fa
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-19 20:32:12 +0530

    Increase max word count for note title

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ApplicationRecord
-  validates :title, length: { minimum: 5, maximum: 14 }
+  validates :title, length: { minimum: 5, maximum: 25 }
   validates :description, length: { maximum: 100 }
 end
