9e5b33193f76801d3e4eebaf2d356df86faf01ae
newhorizon-tech <alaukikinbox@gmail.com>
2021-12-19 18:51:40 +0530

    Add dockerfile and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ruby:2.6.5
+
+ENV BUNDLE_VERSION 2.2.15
+
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
+
+WORKDIR /ror-docker
+COPY . /ror-docker
+
+RUN gem install bundler
+RUN bundle install
+
+EXPOSE 3000
+
+CMD ["bundle", "exec", "rails", "server","-b", "0.0.0.0" ]
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_12_14_133900) do
   enable_extension "plpgsql"
 
   create_table "notes", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
